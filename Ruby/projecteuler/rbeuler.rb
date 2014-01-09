3352717df560d8732a23b5a8447daf25fe070c1b
Isaac Wolff <wolff215@gmail.com>
2014-01-08 18:15:23 -0600

    Added rbeuler

---
+++ Ruby/projecteuler/rbeuler.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require 'set'
+require 'mathn'
+
+
+num = ARGV.shift
+
+def problem46()
+
+  primes = (2..10000).select { |n| n.prime? }.to_set
+  
+  composites = (2..10000).reject { |n| n.prime? }.to_set
+  
+  twicesquares = (0..100).map { |n| 2*(n**2) }.to_set
+  
+  sums = primes.to_a.product(twicesquares.to_a).map { |a,b| a + b }.to_set
+  
+  puts composites.select { |n| !sums.include?(n) and n.odd? }.min
+
+end
+
+eval("problem#{num}()")
